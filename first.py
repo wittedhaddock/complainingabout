535e8e739e234984e5e4d12c22a73782b8f2b9f4
James Graham <wittedhaddock@gmail.com>
2018-08-11 17:33:28 -0400

    pull reddit

--- first.py
+++ first.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+	r = requests.get('https://www.reddit.com/r/all/new/.json', headers = {'User-agent': 'your bot 0.1'})
+	print(r.json())
+	return "yo"
